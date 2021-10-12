a48fb4fce7e27a722fb9387833c268486589f13d
syd645 <56028950+syd645@users.noreply.github.com>
2021-10-12 15:36:25 -0500

    CyberCup21/

---
+++ code_extraction.py
@@ -0,0 +1,22 @@
+#bloos world color code
+
+lines = []
+codes = []
+
+with open('html_base.txt') as f:
+    lines = f.readlines()
+
+
+for line in lines:
+    result = line.index('#')
+    #print(result)
+    slice_size = slice(result+1, result+7,1)
+    code = line[slice_size]
+    #code = str(code)
+    #print(code)
+    code_edit1 = bytes.fromhex(code)
+    code_edit2 = code_edit1.decode()
+    print(code_edit2)
+
+f.close()
+
