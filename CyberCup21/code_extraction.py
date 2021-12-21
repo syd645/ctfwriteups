6a19f9b43d78a3a39c099ed7f36f0c68cb72e226
Sydney <56028950+syd645@users.noreply.github.com>
2021-12-21 11:07:27 -0500

    Update code_extraction.py

--- CyberCup21/code_extraction.py
+++ CyberCup21/code_extraction.py
@@ -2,19 +2,18 @@
 #Getting the hex color codes from the text file holding the HTML
 
 lines = []
-codes = []
 
+#opens the html_base.txt and sets each line as an object in lines array
 with open('html_base.txt') as f:
     lines = f.readlines()
 
-
+#Read in each entry of lines and get only the 6 characters after the '#'
 for line in lines:
     result = line.index('#')
-    #print(result)
     slice_size = slice(result+1, result+7,1)
+    #sets code equal to the needed 6 hex characters
     code = line[slice_size]
-    #code = str(code)
-    #print(code)
+    #converts from hex and prints the output
     code_edit1 = bytes.fromhex(code)
     code_edit2 = code_edit1.decode()
     print(code_edit2)
